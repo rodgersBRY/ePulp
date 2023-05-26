0d65984d759d39a81f83be4d4170da5760b511fb
Brian Mawira <mawirab@kebs.org>
2023-05-26 16:17:33 +0300

    minor changes

--- api/routes/admin.routes.js
+++ api/routes/admin.routes.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 const adminController = require("../controllers/admin.controller");
 
+const authGuard = require("../authguard")
+
 router.get("/get-farmers", adminController.allFarmers);
-router.get("/farmer/:farmerId", adminController.getFarmer);
+router.get("/farmer/:farmerId", authGuard, adminController.getFarmer);
 
-router.post("/create-farmer", adminController.addFarmer);
-router.post("/update-weight/:farmerId", adminController.addFarmerCoffeeWeight);
+router.post("/create-farmer", authGuard, adminController.addFarmer);
+router.post("/update-weight/:farmerId", authGuard, adminController.addFarmerCoffeeWeight);
 
-router.put("/update-farmer/:farmerId", adminController.editFarmer);
+router.put("/update-farmer/:farmerId", authGuard, adminController.editFarmer);
 
-router.delete("/delete-farmer/:farmerId", adminController.deleteFarmer);
+router.delete("/delete-farmer/:farmerId", authGuard, adminController.deleteFarmer);
 
 module.exports = router;
